78643560a16dd273cd2b00022a65b56fab30c9bd
Otto Mättas <otto.mattas@sentia.com>
2019-10-02 22:39:39 +0200

    Upload Lonnie's improved version

--- 1c/stateTransitionFunction.py
+++ 1c/stateTransitionFunction.py
@@ -1,98 +1,91 @@
-def main():
-    print("Hello can I help you")
-    dialogue_state = 'welcome'
-    
-    type = 'None'
+# Methods in AI Research
+# Team project part 1c
+# Maarten San Giorgi, Otto Mättas, David-Paul Niland & Lonnie Bregman
+
+def stateTransition():
+    foodType = 'None'
     area = 'None'
     price = 'None'
     restaurant = 'None'
+    state = 'getInfo'
     
+    print("Welcome! How can I help you?")
     
-    while (dialogue_state != 'exit'):
+    while (state != 'exit'):
         
-        user_utterance = input()
-        inputData = vectorizer.transform({user_utterance})
-        dialog_act = classifier.predict(inputData)[0]
+        # classify the user utterance
+        userUtterance = input()
+        inputData = vectorizer.transform({userUtterance})
+        dialogAct = classifier.predict(inputData)[0]
         
-        if type == 'None':
-            type = getType
+        # you can always exit the dialog with saying goodbye
+        if dialogAct == 'bye':
+            state = 'exit'
+        
+        elif state == 'getInfo':
+            if dialogAct == 'inform':
+                # extract type and/or area and/or price from utterance
+                
+            if foodType == 'None':
+                print("What type of food do you want?")
+                
+            elif area == 'None':
+                print("What area do you want?")
             
-        elif area == 'None':
-            area = getArea
-        
-        elif price == 'None':
-            price = getPrice
+            elif price == 'None':
+                print("What price do you want?")
+                
+            else:
+                state = 'firstProposal'
             
-        else:
-            proposeRestaurant
         
-        
-        
-        
-        
-        
-
-        
-        if dialog_act == 'inform':
-            # Maarten
-        if dialog_act == 'hello':
-        
-        if dialog_act == 'affirm':
+        elif state == 'firstProposal':
+            restaurant = # pick a restaurant from the CSV file
+            print("How about ", restaurant, "?")
+            state = 'nextProposal'
             
-        if dialog_act == 'bye':
+        elif state == 'nextProposal':
+            if dialogAct == 'confirm':
+                print("Thank you. Goodbye!")
+                state = 'exit'
+                
+            elif dialogAct == 'deny' or dialogAct == 'negate' or dialogAct == 'reqmore':
+                restaurant = # pick a different restaurant from the CSV file
+                print("OK, how about ", restaurant, "?")
             
-        if dialog_act == 'confirm':
             
-        if dialog_act == 'deny':
-
-        if dialog_act == 'negate':
             
-        if dialog_act == 'repeat':
             
-        if dialog_act == 'reqalts':
-            
-        if dialog_act == 'reqmore':
-
-        if dialog_act == 'request':
-            
-        if dialog_act == 'restart':
-            
-        if dialog_act == 'thankyou':
-            
-        if dialog_act == 'null':
-  
-        if dialog_act == 'ack'
+stateTransition()          
         
         
-def getType():
-    print("What type of food do you want?")
-    type = # Maarten
-    return type
-
-def getArea():
-    print("What area do you want?")
-    type = # Maarten
-    return area
-    
-def getPrice():
-    print("What price do you want?")
-    type = # Maarten
-    return price
-
-def proposeRestaurant(type, area, price):
-    
-    # csv code
-    
-    return proposal
-
-def getPostCode(restaurant):
-    return postCode
-
-def getNumber(restaurant):
-    return number
-
-def getAddress(restaurant):
-    return address
+        # I left these here for your reference of what dialog acts exist:
         
-
+#        if dialog_act == 'hello':
+#        
+#        if dialog_act == 'affirm':
+#            
+#        if dialog_act == 'bye':
+#            
+#        if dialog_act == 'confirm':
+#            
+#        if dialog_act == 'deny':
+#
+#        if dialog_act == 'negate':
+#            
+#        if dialog_act == 'repeat':
+#            
+#        if dialog_act == 'reqalts':
+#            
+#        if dialog_act == 'reqmore':
+#
+#        if dialog_act == 'request':
+#            
+#        if dialog_act == 'restart':
+#            
+#        if dialog_act == 'thankyou':
+#            
+#        if dialog_act == 'null':
+#  
+#        if dialog_act == 'ack'
         

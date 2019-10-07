99326682338c9168b2ffcf5456a19672e87a218f
Otto Mättas <otto.mattas@gmail.com>
2019-10-08 01:43:29 +0200

    Upload keywordmatching program to the final deliverable

---
+++ 1/1c_keywordMatching.py
@@ -0,0 +1,105 @@
+# Methods in AI Research
+# Team project part 1c - Keyword matching algorithm
+# Maarten San Giorgi, Otto Mättas, David-Paul Niland & Lonnie Bregman
+
+from Levenshtein import distance
+import re
+import json
+
+# all the keywords we can match against, used to fill the keywordList
+keywordsPath = 'C:\\Users\\Maarten\\Documents\\UU master AI\\MAIR\\keywords.json'
+
+# list of keywords to match, filled later in the code
+keywordList = {}
+
+# possible patterns to match
+patternList = {"in the (.*) part of town":["area"],
+               "want (.*) food":["price", "food"],
+               "a (.*) restaurant":["price", "food"],
+               "an (.*) restaurant":["price", "food"],
+               "looking for (.*) food":["price", "food"],
+               "what about (.*) food":["price", "food"],
+               "serve (.*) food":["price", "food"],
+               "serving (.*) food":["price", "food"],
+               "with (.*) food":["price", "food"],
+               "in (.*) area":["area"],
+               "a (.*) priced":["price"],
+               "give me (.*) food":["price", "food"],
+               "(\b.*\b) food":["price", "food"],
+               "(\b.*\b) restaurant":["price", "food"]}
+
+# dictionary to hold the data for known information
+information = {"food":None, "price":None, "area":None}
+
+# Retrieving the JSON data from the keywords file and add them to the keywordList.
+def retrieveKeywordsFromJSON():
+    with open(keywordsPath) as keywordsJSON:
+        file = keywordsJSON.read()
+
+    keywordData = json.loads(file)
+    for foodtype in keywordData["informable"]["food"]:
+        keywordList[foodtype] = "food"
+    for price in keywordData["informable"]["pricerange"]:
+        keywordList[price] = "price"
+    for area in keywordData["informable"]["area"]:
+        keywordList[area] = "area"
+
+# Matching keywords against words (for certain types) using the Levenshtein with a certain threshold.
+def keywordMatching(sentence, grade, types):
+    words = sentence.split()
+
+    for word in words:
+        for keyword in keywordList:
+            dist = distance(word.lower(), keyword.lower())
+            if dist < grade and keywordList[keyword] in types:
+                if dist == 0:
+                    information[keywordList[keyword]] = keyword
+                else:
+                    check = input("You said " + word + ". Did you mean " + keyword + "?(yes/no)\n")
+                    if (check == "yes"):
+                        information[keywordList[keyword]] = keyword
+                    else:
+                        continue
+
+# Matching patterns against a sentence.
+def patternMatching(sentence):
+    for pattern in patternList:
+        search = re.search(pattern.lower(), sentence.lower())
+        if search:
+            keywordMatching(search.group(1), 3, patternList[pattern])
+
+# Function that returns true if all information is known, else false.
+def haveAllInformation():
+    for element in information:
+        if information[element] == None:
+            return False
+
+    return True
+
+# Function that returns all the information we still need.
+def getNeededInformation():
+    result = ""
+    for element in information:
+        if information[element] == None:
+            result += element + " and "
+
+    if result.endswith(" and "):
+        result = result[:-5]
+
+    return result
+
+# Main function.
+def main():
+    retrieveKeywordsFromJSON()
+
+    while not haveAllInformation():
+            test = input("random input sentence (type 'exit' to quit): ")
+            if test == "exit":
+                break
+            patternMatching(test)
+            keywordMatching(test, 1, ["price", "area", "food"])
+            print ("give me more information about " + getNeededInformation())
+
+    print(information)
+
+main()
